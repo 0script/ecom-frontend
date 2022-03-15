20c319f1949fcd187babea06e6bc17c851748faf
0script <obmco76@gmail.com>
2022-03-15 16:00:03 +0200

    Next Add To Cart Function

--- public/js/product.js
+++ public/js/product.js
@@ -1,9 +1,33 @@
 const imgs=document.querySelectorAll('img.product-img');
+const add_btns=document.querySelectorAll('button.add-to-cart');
 
-imgs.forEach((img)=>{
+class CartItem {
+    constructor(productname,price,qtt){
+        this.name=productname;
+        this.price=price;
+        this.quantity=qtt;
+    }
+    
+    //getter
+    get name(){
+        return this.name;
+    }
 
-    img.parentElement.addEventListener('mouseenter',()=>{
-        console.log('enter');
+    get price(){
+        return this.price;
+    }
+
+    get quantity(){
+        return this.quantity;
+    }
+
+    //method
+}
+
+imgs.forEach(img=>{
+    /* Display the details when mouse is over the img cart item */
+
+    img.addEventListener('mouseenter',(e)=>{
         img.classList.add('not');
         img.parentElement.firstElementChild.classList.add('display');
 
@@ -13,4 +37,9 @@ imgs.forEach((img)=>{
         });
     });
 
+});
+
+/* function to add item to cart*/
+add_btns.forEach(btn => {
+    /* will create a local variable that will be a list of CartItem object*/
 });
